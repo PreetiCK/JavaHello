2d0b6081b196379e9aa8641c2101eb759a726812
Preeti Khodanpur <preetikhodanpur038@gmail.com>
2015-10-26 13:49:46 +0530

    add Demo.java

---
+++ src/main/java/com/mindtree/Demo.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.mindtree;
+
+/**
+ * @author M1017042
+ *
+ */
+public class Demo {
+
+	/**
+	 * 
+	 */
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		int b;
+		
+		System.out.println("Hello World");
+		
+		
+
+	}
+
+}
